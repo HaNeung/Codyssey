3489be64723d7bcd345655d0b6f9931e4a4c298b
HaNeung <smdaos112@naver.com>
2025-03-25 11:39:21 +0900

    과제

---
+++ main.py
@@ -0,0 +1,20 @@
+try:
+    with open('./mission_computer_main.log', 'r', encoding='UTF-8',) as file:
+        logList = file.readlines()
+
+    for log in logList:
+        print(log, end='')
+
+    print('\n------------------')
+
+    logList.reverse()
+
+    for log in logList:
+        print(log, end='')
+
+
+except Exception as e:
+    error_message = str(e)
+    with open('./error_file.txt', 'w') as error_file:
+        error_file.write(error_message)
+    
