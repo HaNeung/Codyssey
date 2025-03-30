47a775a27b7157c9a89380c9a852d4d1e898fac5
HaNeung <smdaos112@naver.com>
2025-03-31 00:08:45 +0900

    과제

--- main.py
+++ main.py
@@ -1,22 +1,52 @@
-print('Hello Mars')
-
-try:
-    with open('./mission_computer_main.log', 'r', encoding='UTF-8',) as file:
-        logList = file.readlines()
-
-    for log in logList:
-        print(log, end='')
-
-    print('\n------------------')
-
-    logList.reverse()
-
-    for log in logList:
-        print(log, end='')
+log_list =[]
 
 
-except Exception as e:
-    error_message = str(e)
-    with open('./error_file.txt', 'w') as error_file:
-        error_file.write(error_message)
+file=open('./mission_computer_main.log', 'r')
+
+        
+for line in file:
+            parts = line.strip().split(",")
+         
+
+            if len(parts) >= 3:
+                timestamp = parts[0].strip()
+                log_type = parts[1].strip()
+                message = ",".join(parts[2:]).strip()
+         
+                log_list.append([timestamp, log_type, message])
+                
+print(log_list) 
+                
+                
+log_list.sort(reverse=True)
     
+for l in log_list:
+        print(l)
+
+log_dic = {log[0]: {"log_type": log[1], "message": log[2]} for log in log_list}
+    
+print(log_dic)
+    
+file_2= open('mission_computer_main.json', 'w') 
+json_dump= '{\n'
+for key, value in log_dic.items(): 
+        json_dump +='    "'+ key + '": {\n' 
+        json_dump +='    "log_type": "'+ value['log_type'] + '", \n'
+        json_dump +='    "message": "'+ value['message'] + '"\n'
+        json_dump +='    },\n'
+
+
+json_dump = json_dump.strip(',\n') +'\n}'
+
+file_2.write(json_dump) 
+
+
+file.close()
+file_2.close()
+    
+    
+  
+    
+    
+                
+
